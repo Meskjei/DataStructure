b382fa7c8d43466efd597687cc5f82cdf2cae67f
Meskjei <513087078@qq.com>
2019-05-18 15:49:39 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by 黄嘉成 on 2019/5/18.
+//  Copyright © 2019 黄嘉成. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
